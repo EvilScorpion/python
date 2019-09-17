53f0d671de760dd0531352ef8011b226871e18a6
Tsoy Vyacheslav <zoyberk1991@gmail.com>
2019-09-17 17:08:49 +0500

    change

--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-a = 5
-b = 3
-c = a + b
-print(c)
+# Словари
+
+# Словарь №1
+d = {'test': 99, 'start': 0}
+print(d['test'])
+
+# Словарь №2
+
+dic = dict(short='first', next='time')
+print(dic['next'], d['start'])
+
+# Словарь №3 для каждого ключа несколько одинаковых элементов.
+
+dictationary = dict.fromkeys(['a', 'b', 'c'], ['55', 'root', 'last'])
+print(dictationary['b'])
+
+
+for i in d:
+    print(i)
+
+while d['test'] >= 100:
+    print(d)
